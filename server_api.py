2a82d251f8e821d8d696ddfbb2cc84eb013c2a3e
Sanchez, Nahir Gamaliel (Nahir) <NAHIR.GAMALIEL.SANCHEZ@ROCKWELLCOLLINS.COM>
2021-04-28 12:15:27 -0700

    Removed empty file

--- server_api.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-from bottle import run, post, request, response, get, route
-
-@route('/<path>',method = 'GET')
-def process(path):
-    return subprocess.check_output(['python3',path+'.py'],shell=True)
-
-run(host='localhost', port=8080, debug=True)
