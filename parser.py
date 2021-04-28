8d1547c46d6303ff77401dc9e4e1055e6abf2a2f
Sanchez, Nahir Gamaliel (Nahir) <NAHIR.GAMALIEL.SANCHEZ@ROCKWELLCOLLINS.COM>
2021-04-28 10:01:53 -0700

    Added parser to consume from page into a json api.

---
+++ parser.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import subprocess
+from bottle import run, post, request, response, get, route
+from json import dumps
+
+table_content = []
+def parse():
+    url = 'https://www.tsa.gov/coronavirus/passenger-throughput'
+    NO_PROXY = {
+        'no': 'pass',
+    }
+    page = requests.get(url,proxies=NO_PROXY)
+    soup = BeautifulSoup(page.content, "html.parser")
+    rows = soup.find_all("table")[0].find("tbody").find_all("tr")
+
+    for row in rows:
+        cells = row.find_all("td")
+        json_object = {'date':cells[0].get_text().strip(), 'travel_21':cells[1].get_text().strip(), 'travel_20':cells[2].get_text().strip(), 'travel_19': cells[3].get_text().strip()}
+        table_content.append(json_object)
+    return table_content
+
+@route('/parser',method = 'GET')
+def process():
+    response.content_type = 'application/json'
+    data = parse()
+    return dumps(data)
+
+run(host='localhost', port=8080, debug=True)
